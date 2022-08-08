c0d9151533f82acac211261537200904e85a9a4c
ANTON <19111984@list.ru>
2022-08-08 20:19:00 +0700

    new task

---
+++ Task0/Program.cs
@@ -0,0 +1,8 @@
+﻿// программа на входе принимает число
+// на выходе выдает его квадрат
+// 4 - 16; -3 - 9; -7 - 49
+Console.WriteLine("Enter int number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine($"Square number {num} = {square}");
+
