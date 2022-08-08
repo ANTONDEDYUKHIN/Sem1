b35cc05d9cb5b28b9a733cf324b19de156d61b08
ANTON <19111984@list.ru>
2022-08-08 21:09:53 +0700

    -N:N

---
+++ Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// на входе число Н
+// на выходе ряд чиесл от -Н до Н
+// 2 - -2,-1,0,1,2
+Console.Write("Enter int number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Print numbers in diap -N:N :");
+for (int i = -a; i <= a; i++);
+{
+    Console.Write(i + " ");
+}
