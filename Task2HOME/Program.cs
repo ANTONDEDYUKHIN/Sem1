e30224fcada89c03fe4782bf6bdd4969c34aff03
ANTON <19111984@list.ru>
2022-08-08 22:17:37 +0700

    comparing numbers

---
+++ Task2HOME/Program.cs
@@ -0,0 +1,9 @@
+﻿// на входе 2 числа
+// на выход выдает какое больше, какое меньше
+// a=5; b=7 max 7
+Console.Write("Введите число a= ");
+int a = Convert.ToInt32(System.Console.ReadLine());
+Console.Write("Введите число b= ");
+int b = Convert.ToInt32(System.Console.ReadLine());
+if (a > b) Console.WriteLine($"Число {a} больше, Число {b} меньше");
+else Console.WriteLine($"Число {b} больше, Число {a} меньше");
