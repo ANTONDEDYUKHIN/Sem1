963d8d49b6f699c047d12acad571d7a84617a96a
ANTON <19111984@list.ru>
2022-08-08 23:00:21 +0700

    Max in 3 numbers1vers

--- Task4HOME/Program.cs
+++ Task4HOME/Program.cs
@@ -5,9 +5,8 @@ Console.Write("Введите число a= ");
 int a = Convert.ToInt32(System.Console.ReadLine());
 Console.Write("Введите число b= ");
 int b = Convert.ToInt32(System.Console.ReadLine());
-Console.Write("Введите число b= ");
-int c = Convert.ToInt32(System.Console.ReadLine());
 Console.Write("Введите число c= ");
+int c = Convert.ToInt32(System.Console.ReadLine());
 if (a > b) 
 {
     a = max;
