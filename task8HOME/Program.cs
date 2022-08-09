b57c9e43995ce7cd07bc769c0e3c7705e5dc6f02
ANTON <19111984@list.ru>
2022-08-09 20:22:21 +0700

    even num 2-N

---
+++ task8HOME/Program.cs
@@ -0,0 +1,12 @@
+﻿// на входе число Н
+// на выходе ряд четных чисел от 1 до Н
+// 5 - 2, 4
+Console.Write("Enter int number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("All numbers of interval 1-N: ");
+for (int i = 2; i <= N; i++)
+if (i % 2 == 0)
+{
+    Console.Write(i +", ");
+}
