24b1d38ad9b1e394162d36de4b3f92358116fe3f
ANTON <19111984@list.ru>
2022-08-09 19:39:56 +0700

    Чет-нечет

---
+++ Task6HOME/Program.cs
@@ -0,0 +1,13 @@
+﻿// на входе вводится число
+// программа проверяет четное или нет
+Console.Write("Введите число N= ");
+int N = Convert.ToInt32(System.Console.ReadLine());
+
+if (N % 2 == 0)
+{
+Console.WriteLine($"число {N} четное");
+}
+else
+{
+   Console.WriteLine($"число {N} нечетное"); 
+};
